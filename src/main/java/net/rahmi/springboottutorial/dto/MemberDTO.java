4cfeea8f6017964e234e6e419d39a4f94752f6ae
Rachid RAHMI <rachid.rahmi@gmail.com>
2023-10-19 21:48:04 +0200

    Added Get Controller

---
+++ src/main/java/net/rahmi/springboottutorial/dto/MemberDTO.java
@@ -0,0 +1,23 @@
+package net.rahmi.springboottutorial.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MemberDTO 
+{
+	private String name;
+    private String email;
+    private String organization;
+    
+	@Override
+	public String toString() {
+		return "MemberDTO {"
+				+ "name='" + name + '\'' +
+				", email='" + email + '\'' +
+				", organization='" + organization + '\'' +
+				'}';
+	}
+
+}
