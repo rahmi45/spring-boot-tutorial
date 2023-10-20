70300b509aca9f4fcc414273c3b72f098b6e2c79
Rachid RAHMI <rachid.rahmi@gmail.com>
2023-10-20 20:27:27 +0200

    Added Put Controller

---
+++ src/main/java/net/rahmi/springboottutorial/controller/PutController.java
@@ -0,0 +1,58 @@
+package net.rahmi.springboottutorial.controller;
+
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import net.rahmi.springboottutorial.dto.MemberDTO;
+
+@RestController
+@RequestMapping("/api/v1/put-api")
+public class PutController {
+	
+	//http://localhost:9090/api/v1/put-api/default
+    @PutMapping(value = "/default")
+    public String putMethod() {
+        return "Hello put!";
+    }
+    
+    //http://localhost:9090/api/v1/put-api/member
+    @PutMapping(value = "/member")
+    public String postMember(@RequestBody Map<String, Object> putData) {
+        StringBuilder sb = new StringBuilder();
+
+        putData.entrySet().forEach(map -> {
+            sb.append(map.getKey() + " : " + map.getValue() + "\n");
+        });
+
+    /*
+    param.forEach((key, value) -> sb.append(key).append(" : ").append(value).append("\n"));
+     */
+
+        return sb.toString();
+    }
+    
+    // http://localhost:9090/api/v1/put-api/member1
+    @PutMapping(value = "/member1")
+    public String postMemberDto1(@RequestBody MemberDTO memberDTO) {
+        return memberDTO.toString();
+    }
+
+    // http://localhost:9090/api/v1/put-api/member2
+    @PutMapping(value = "/member2")
+    public MemberDTO postMemberDto2(@RequestBody MemberDTO memberDTO) {
+        return memberDTO;
+    }
+    
+    // http://localhost:9090/api/v1/put-api/member3
+    @PutMapping(value = "/member3")
+    public ResponseEntity<MemberDTO> postMemberDto3(@RequestBody MemberDTO memberDTO) {
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(memberDTO);
+    }
+
+}
