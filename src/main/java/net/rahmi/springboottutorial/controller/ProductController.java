5ad93665cd3ff3e65b2671c5c475c9589279ad57
Rachid RAHMI <rachid.rahmi@gmail.com>
2023-10-22 20:44:32 +0200

    Added Spring Boot Validation (Hibernate-Validator)

--- src/main/java/net/rahmi/springboottutorial/controller/ProductController.java
+++ src/main/java/net/rahmi/springboottutorial/controller/ProductController.java
@@ -1,8 +1,12 @@
 package net.rahmi.springboottutorial.controller;
 
+import javax.validation.Valid;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -33,23 +37,44 @@ public class ProductController
 	    LOGGER.info("[ProductController] perform {} of Spring-boot API.", "getProduct");
 	    
 	    ProductDto productDto = productService.getProduct(productId);
-	    LOGGER.info("[ProductController] Response :: productId = {}, productName = {}, productPrice = {}, productStock = {}, Response Time = {}ms", productDto.getProductId(),
-	            productDto.getProductName(), productDto.getProductPrice(), productDto.getProductStock(), (System.currentTimeMillis()- startTime));
+	    
+	    LOGGER.info(
+	            "[ProductController] Response :: productId = {}, productName = {}, productPrice = {}, productStock = {}, Response Time = {}ms",
+	            productDto.getProductId(),
+	            productDto.getProductName(), 
+	            productDto.getProductPrice(), 
+	            productDto.getProductStock(),
+	            (System.currentTimeMillis() - startTime));
+	    
 	    return productDto;
 	    
 	  }
 	  
 	  // http://localhost:8080/api/v1/product-api/product
 	  @PostMapping(value = "/product")
-	  public ProductDto createProduct(@RequestBody ProductDto productDto) 
+	  public ResponseEntity<ProductDto> createProduct(@Valid @RequestBody ProductDto productDto) 
 	  {
-
+		// Validation Code 
+	    if (productDto.getProductId().equals("") || productDto.getProductId().isEmpty()) {
+	      LOGGER.error("[createProduct] failed Response :: productId is Empty");
+	      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(productDto);
+	    }
 	    String productId = productDto.getProductId();
 	    String productName = productDto.getProductName();
 	    int productPrice = productDto.getProductPrice();
 	    int productStock = productDto.getProductStock();
+	    
+	    ProductDto response = productService
+	            .saveProduct(productId, productName, productPrice, productStock);
+	    
+	    LOGGER.info(
+	            "[createProduct] Response >> productId : {}, productName : {}, productPrice : {}, productStock : {}",
+	            response.getProductId(), 
+	            response.getProductName(), 
+	            response.getProductPrice(),
+	            response.getProductStock());
+	    return ResponseEntity.status(HttpStatus.OK).body(response);
 
-	    return productService.saveProduct(productId,productName,productPrice,productStock);
 	  }
 	  
 	  // http://localhost:8080/api/v1/product-api/product/{productId}
--- src/main/java/net/rahmi/springboottutorial/data/dto/ProductDto.java
+++ src/main/java/net/rahmi/springboottutorial/data/dto/ProductDto.java
@@ -1,5 +1,9 @@
 package net.rahmi.springboottutorial.data.dto;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,10 +17,21 @@ import net.rahmi.springboottutorial.data.entity.Product;
 @ToString
 @Builder
 public class ProductDto 
-{
+{   
+	@NotNull
 	private String productId;
+	
+	@NotNull
 	private String productName;
+	
+	@NotNull
+	@Min(value = 500)
+	@Max(value = 3000000)
 	private int productPrice;
+	
+	@NotNull
+	@Min(value = 0)
+	@Max(value = 9999)
 	private int productStock;
 	
 	public Product toEntity()
